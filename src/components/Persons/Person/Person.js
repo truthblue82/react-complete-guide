b881b8fa61ad97e4c3610079f3af2fceef38c224
truthblue82 <truthblue82@gmail.com>
2020-01-06 16:19:36 +0700

    deep component and module

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,11 +4,7 @@ import styled from 'styled-components';
 import classes from './Person.css'
 
 const person = (props) => {
-    const rnd = Math.random();
-
-    if (rnd > 0.7) {
-        throw new Error( 'Something went wrong' );
-    }
+    
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} year old!</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 import classes from './App.css';
 
-import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person'
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -51,15 +51,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary>
-              <Person
+            return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
               key={person.id}
               changed={(event) => this.nameChangedHandler(event, person.id)}
               />
-              </ErrorBoundary>
           })}
         </div>
       );
